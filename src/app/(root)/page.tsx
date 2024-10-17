8526f1c9b6885ad61dd510b4551b9c3ae0d2cb24
kwakjaehun <jh5414092@icloud.com>
2024-10-17 22:07:10 +0900

    Feat: 메인 페이지 작성

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import ImageContainer from "@/components/ImageContainer/ImageContainer";
+
+export default function Home() {
+  return (
+    <main className="h-screen w-full flex justify-center items-center">
+      <ImageContainer />
+    </main>
+  );
+}
