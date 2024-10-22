20f106f83759ca0e57efa15adb56d9fb189a9517
kwakjaehun <jh5414092@icloud.com>
2024-10-22 16:33:11 +0900

    Add PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+import nextPWA from "next-pwa";
+
+const withPWA = nextPWA({
+  dest: "public",
+});
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
